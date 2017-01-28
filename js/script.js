d9107e943d01b4f7374e9b9dbc0a51ded4165b4f
Oromar Dantas <oromar.melo@gmail.com>
2017-01-28 00:40:08 -0300

    Added bg image from Google Street View

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
 
 function loadData() {
 
+	var streetViewURL =  "http://maps.googleapis.com/maps/api/streetview?size=1024x768&location=";
+
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
-
+	
     // clear out old data before new request
     $wikiElem.text("");
-    $nytElem.text("");
-
+    $nytElem.text("");	
+	
     // load streetview
-
-    // YOUR CODE GOES HERE!
-
+	// YOUR CODE GOES HERE!
+	
+	var $street = $('#street');
+	var $city = $('#city');	
+	var result = streetViewURL + $street.val() + ", " + $city.val();
+	$body.append('<img class="bgimg" src="' + result + '">');
     return false;
 };
 
